597417bc096ac58a60a16911435dec9cb161feec
Josef Ore <71660137@continental.edu.pe>
2025-04-09 11:20:01 -0500

    Se agregó las funciones y atributo  de la clase y se realizó el ingreso en el main.

--- circulo.py
+++ circulo.py
@@ -1,2 +1,11 @@
 class Circulo:
-    pass
+    pi = 3.1416
+    def __init__(self, radio):
+        self.radio = radio
+
+    def circunferencia(self):
+        return (2*self.pi*self.radio)
+
+if __name__=='__main__':
+    instancia = Circulo(float(input("Ingrese el radio: ")))
+    print(f"La circunferencia de un circulo de radio {instancia.radio} es: {instancia.circunferencia()}")
