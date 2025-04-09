d6bd0103daddf4267ca186a451ebe88b1842f851
Josef Ore <71660137@continental.edu.pe>
2025-04-09 11:35:52 -0500

    Se agregó la función área a circulo y una salida para ver el resultado en if main

--- circulo.py
+++ circulo.py
@@ -6,6 +6,10 @@ class Circulo:
     def circunferencia(self):
         return (2*self.pi*self.radio)
 
+    def area(self):
+        return self.pi*self.radio**2
+
 if __name__=='__main__':
     instancia = Circulo(float(input("Ingrese el radio: ")))
     print(f"La circunferencia de un circulo de radio {instancia.radio} es: {instancia.circunferencia()}")
+    print(f"El radio de un circulo de radio {instancia.radio} es: {instancia.area()}")
